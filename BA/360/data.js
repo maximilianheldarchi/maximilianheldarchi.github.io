58b8db2ad1e9be011f60809577b3a8aa975f145b
maximilianheldarchi <68516140+maximilianheldarchi@users.noreply.github.com>
2020-07-22 01:55:00 +0200

    test4
    I A K I Maximilian Held

--- BA/360/data.js
+++ BA/360/data.js
@@ -26,50 +26,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0,
-          "pitch": 0,
+          "yaw": -0.18011425559863525,
+          "pitch": -0.0914993098434671,
           "rotation": 0,
-          "target": "1-panorama7"
+          "target": "1-panorama8"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-panorama7",
-      "name": "Panorama7",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0,
-          "pitch": 0,
-          "rotation": 0,
-          "target": "2-panorama8"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-panorama8",
+      "id": "1-panorama8",
       "name": "Panorama8",
       "levels": [
         {
